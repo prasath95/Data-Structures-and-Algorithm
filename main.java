ae34d6412f19fd510e9a5b0392ceac115a700894
Siva Prasath <6sivaprasath@gmail.com>
2023-11-06 14:14:08 +0530

    stack implementation

--- main.java
+++ main.java
@@ -1,9 +1,27 @@
 /**
  * main
  */
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        Stack obj = new Stack();
+        obj.push(1);
+        obj.push(2);
+        obj.push(3);
+       
+        // System.out.println("pop");
+        // obj.pop();
+
+        System.out.println("show peek "+obj.peek());
+
+        obj.show();
+        
+
+        
+       
+
     }
+
 }
